569d7eb76dce65fa86f92d9fcdd766450b012479
Real Phoenix <51527258+rsktg@users.noreply.github.com>
2020-08-31 23:09:40 +0530

    Improve blacklisting users
    Don't let blacklisted users use MessageHandler or RegexHandler

--- tg_bot/modules/helper_funcs/handlers.py
+++ tg_bot/modules/helper_funcs/handlers.py
@@ -1,3 +1,4 @@
+import re
 import telegram.ext as tg
 from telegram import Update
 import tg_bot.modules.sql.blacklistusers_sql as sql
@@ -15,8 +16,9 @@ class CustomCommandHandler(tg.CommandHandler):
         if (isinstance(update, Update)
                 and (update.message or update.edited_message and self.allow_edited)):
             message = update.message or update.edited_message
-            if sql.is_user_blacklisted(update.effective_user.id):
-                return False
+            if update.effective_user:
+                if sql.is_user_blacklisted(update.effective_user.id):
+                    return False
 
             if message.text and len(message.text) > 1:
                 fst_word = message.text_html.split(None, 1)[0]
@@ -39,3 +41,43 @@ class CustomCommandHandler(tg.CommandHandler):
 class CustomRegexHandler(tg.RegexHandler):
     def __init__(self, pattern, callback, friendly="", **kwargs):
         super().__init__(pattern, callback, **kwargs)
+
+    def check_update(self, update):
+        if isinstance(update, Update) and update.effective_message:
+            if update.effective_user:
+                if sql.is_user_blacklisted(update.effective_user.id):
+                    return False
+        else:
+            return False
+        if any([self.message_updates and update.message,
+                    self.edited_updates and (update.edited_message or update.edited_channel_post),
+                    self.channel_post_updates and update.channel_post]) and \
+                    update.effective_message.text:
+                match = re.match(self.pattern, update.effective_message.text)
+                return bool(match)
+        return False
+
+
+class CustomMessageHandler(tg.MessageHandler):
+    def __init__(self, filters, callback, **kwargs):
+        super().__init__(filters, callback, **kwargs)
+        
+    def check_update(self, update):
+        if isinstance(update, Update) and self._is_allowed_update(update):
+            if update.effective_user:
+                if sql.is_user_blacklisted(update.effective_user.id):
+                    return False
+            if self.filters is None:
+                res = True
+                
+            else:
+                message = update.effective_message
+                if isinstance(self.filters, list):
+                    res = any(func(message) for func in self.filters)
+                else:
+                    res = self.filters(message)
+                    
+        else:
+            res = False
+            
+        return res
