a59b5d2db2c6e63fcf1e0c49f977858bcb8436b8
Real Phoenix <phoenix@therealphoenix.ga>
2020-12-16 10:15:05 +0100

    [translate] Translate caption of media messages

--- tg_bot/modules/gtranslator.py
+++ tg_bot/modules/gtranslator.py
@@ -14,18 +14,22 @@ async def translate(_, message: Message) -> None:
     if not reply_msg:
         await message.reply_text("Reply to a message to translate it!")
         return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
     try:
         args = message.text.split()[1].lower()
         if "//" in args:
             source = args.split("//")[0]
             dest = args.split("//")[1]
         else:
-            source = await trans.detect(reply_msg.text)
+            source = await trans.detect(to_translate)
             dest = args
     except IndexError:
-        source = await trans.detect(reply_msg.text)
+        source = await trans.detect(to_translate)
         dest = "en"
-    translation = await trans(reply_msg.text,
+    translation = await trans(to_translate,
                               sourcelang=source, targetlang=dest)
     reply = f"<b>Translated from {source} to {dest}</b>:\n" \
         f"<code>{translation.text}</code>"
