1e51eeed08726a7343529122253caf55d5185cb2
Sayan Biswas <sayan@pokurt.me>
2021-08-26 15:55:21 +0530

    Use SyncTranslator for translation and use ptb in place of pyrogram
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- tg_bot/modules/gtranslator.py
+++ tg_bot/modules/gtranslator.py
@@ -1,18 +1,16 @@
-from gpytranslate import Translator
-from pyrogram import filters
-from pyrogram.types import Message
+from gpytranslate import SyncTranslator
+from telegram import ParseMode, Update, Bot
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
 
-from tg_bot import pg
-
-
-trans = Translator()
+trans = SyncTranslator()
 
 
-@pg.on_message(filters.command(["tl", "tr"]))
-async def translate(_, message: Message) -> None:
+def translate(bot: Bot, update: Update) -> None:
+    message = update.effective_message
     reply_msg = message.reply_to_message
     if not reply_msg:
-        await message.reply_text("Reply to a message to translate it!")
+        message.reply_text("Reply to a message to translate it!")
         return
     if reply_msg.caption:
         to_translate = reply_msg.caption
@@ -24,26 +22,26 @@ async def translate(_, message: Message) -> None:
             source = args.split("//")[0]
             dest = args.split("//")[1]
         else:
-            source = await trans.detect(to_translate)
+            source = trans.detect(to_translate)
             dest = args
     except IndexError:
-        source = await trans.detect(to_translate)
+        source = trans.detect(to_translate)
         dest = "en"
-    translation = await trans(to_translate,
-                              sourcelang=source, targetlang=dest)
+    translation = trans(to_translate,
+                        sourcelang=source, targetlang=dest)
     reply = f"<b>Translated from {source} to {dest}</b>:\n" \
         f"<code>{translation.text}</code>"
 
-    await message.reply_text(reply, parse_mode="html")
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
 
 
-@pg.on_message(filters.command("langs"))
-async def languages(_, message: Message) -> None:
-    await message.reply_text(
+
+def languages(bot: Bot, update: Update) -> None:
+    message = update.effective_message
+    message.reply_text(
         "Click [here](https://cloud.google.com/translate/docs/languages)"
         " to see the list of supported language codes!",
-        disable_web_page_preview=True
-    )
+        disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 
 __mod_name__ = "Translation"
@@ -61,3 +59,11 @@ eg: `/tl ja`: translates to Japanese.
 eg: `/tl ja//en`: translates from Japanese to English.
 
 • `/langs`: get a list of supported languages for translation."""
+
+
+
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tl", "tr"], translate)
+LANGUAGES_HANDLER = DisableAbleCommandHandler("langs", languages)
+dispatcher.add_handler(TRANSLATE_HANDLER)
+dispatcher.add_handler(LANGUAGES_HANDLER)
+
